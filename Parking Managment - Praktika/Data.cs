a14ce4e70f0ea0969343cb455ba46f89b68a472e
kkkristiyana <kknikolova_@abv.bg>
2025-07-02 09:22:24 +0300

    Constructor done

---
+++ Parking Managment - Praktika/Data.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Parking_Managment___Praktika
+{
+    using System.Text.Json;
+    using static Constants;
+    internal class Data
+    {
+        public List<Book> Books { get; private set; }
+
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        public Data()
+        {
+            LoadBooks();
+        }
+
+        public void Save()
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            using (writer)
+            {
+                string jsonData = JsonSerializer.Serialize(Books);
+                writer.Write(jsonData);
+            }
+        }
+
+        public void LoadBooks()
+        {
+            reader = new StreamReader(filePath);
+            using (reader)
+            {
+                string jsonData = reader.ReadToEnd();
+                Books = JsonSerializer.Deserialize<List<Book>>(jsonData)!;
+            }
+            Books ??= new List<Book>();
+        }
+    }
+}
---
+++ Parking Managment - Praktika/Parking.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking_Managment___Praktika
+{
+    internal class Parking
+    {
+        public int ParkingID { get; set; }
+
+        public string Loc { get; set; }
+        
+        public int TotalSpaces { get; set; }
+
+        public int AvailableSpaces { get; set; }
+
+        public string Vehicles { get; set; }
+
+        public Parking(string loc,int totalSpaces)
+        {
+            Loc = loc;
+            TotalSpaces = totalSpaces;
+            AvailableSpaces = totalSpaces;
+        }
+    }
+}
