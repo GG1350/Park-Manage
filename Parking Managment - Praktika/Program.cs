24dd8a35e8f23d0ce7eabd8cff654fada6b84581
Георги Захариев <gocko1305@gmail.com>
2025-07-07 14:14:34 +0300

    bare switch added

--- Parking Managment - Praktika/Program.cs
+++ Parking Managment - Praktika/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Parking_Managment___Praktika
 {
@@ -11,6 +10,36 @@ namespace Parking_Managment___Praktika
 
             DisplayMenu();
 
+            string choice;
+            while ((choice = Console.ReadLine()) != "x")
+            {
+                switch(choice)
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    case "4":
+
+                        break;
+                    case "5":
+
+                        break;
+                    case "6":
+
+                        break;
+                    case "7":
+
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
         private static void DisplayMenu()//The Menu from which the user will control the program
         {
