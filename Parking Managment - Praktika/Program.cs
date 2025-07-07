8a9f10a4b0b23f1944f55318a974e4c745b0b3f7
Георги Захариев <gocko1305@gmail.com>
2025-07-07 14:01:18 +0300

    Menu done and added the needed methods(they needa be done)

--- Parking Managment - Praktika/Program.cs
+++ Parking Managment - Praktika/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Parking_Managment___Praktika
 {
@@ -6,13 +7,64 @@ namespace Parking_Managment___Praktika
     {
         static void Main(string[] args)
         {
-              
+            SetIOEncoding();
+
+            DisplayMenu();
+
         }
-        
-        private static Parking DisplayAddBookUI()
+        private static void DisplayMenu()//The Menu from which the user will control the program
         {
-            // TODO: Implement Display adding of a new book
-            throw new NotImplementedException();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("==================[ М Е Н Ю ]===================");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("|  [1] ▶ Добавяне на паркинг                   |");
+            Console.WriteLine("|  [2] ▶ Добавяне на ново превозно средсто     |");
+            Console.WriteLine("|  [3] ▶ Премахване на превозно средство       |");
+            Console.WriteLine("|  [4] ▶ Спрaвка за наличните паркоместа       |");
+            Console.WriteLine("|  [5] ▶ Справка за всички паркинги            |");
+            Console.WriteLine("|  [6] ▶ Справка за посочен паринг             |");
+            Console.WriteLine("|  [7] ▶ Проверка за паркинги с еднаква локация|");
+            Console.WriteLine("|  [x] ▶ Изход                                 |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("================================================");
+            Console.ResetColor();
+            Console.Write("> Изберете опция: ");
+            
+        }
+        private static void AddNewParkingUI()//Adds new parking with data specified by the user
+        {
+
+        }
+        private static void RegisterVehicleInParkingUI()//registers a new Vehicle with a plate number specified by the user
+        {
+
+        }
+        private static void RemoveVehicleFromParkingUI()//removes a vehicle with plate number specified by the user
+        {
+
+        }
+        private static void CheckAvailableParkingSpacesUI()//checks how much available spaces are there in a said parking
+        {
+
+        }
+        private static void ReportForAllParkingsUI()//gives a report for the full info of each parking
+        {
+
+        }
+        private static void CheckSpecificParkingUI()//gives a full info for a specified by the user parking
+        {
+
+        }
+        private static void CheckForParkingsWithSameLocUI()//checks for parkings with the same location
+        {
+
+        }
+
+        private static void SetIOEncoding()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
         }
 
     }
